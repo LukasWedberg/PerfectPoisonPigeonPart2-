d6a2111f8af4fc5216f0e526ba9251ecc89e2bd9
LukasWedberg <131009406+LukasWedberg@users.noreply.github.com>
2024-04-14 23:04:36 -0400

    Guess we better call it a day. Heavily modified the camera so that it fits both the player and the boss on the screen. Now I just have to make it so that it also rotates around the player. Also fixed some kinks with the character controller.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField]
     Transform target = null;
+    
+    
+    [SerializeField]
+    Transform target2 = null;
+
+    
 
     PigeonController playerScript;
 
@@ -19,7 +24,19 @@ public class CameraManager : MonoBehaviour
     float minCameraDistance = 2;
 
 
+    [SerializeField]
+    float minFollowDistance = 2;
+
+    [SerializeField]
+    float extraZoomDist = 10;
+
+
+    public float playerFocus = .5f;
+
+
     Quaternion targetRot;
+
+    private Vector3 targetPoint;
 
 
     // Start is called before the first frame update
@@ -42,15 +59,54 @@ public class CameraManager : MonoBehaviour
         //Debug.Log(playerScript.currentPigeonState == PigeonController.PigeonState.Respawning);
         //Debug.Log(playerScript.currentPigeonState == PigeonController.PigeonState.Respawning);
 
+
+
         
 
         if (playerScript.currentPigeonState != PigeonController.PigeonState.Respawning)
         {
-            transform.position = new Vector3(transform.position.x, target.position.y + heightAbovePlayer, transform.position.z);
+            Bounds targetBounds = new Bounds(target.position, Vector3.zero);
 
-            Vector3 originDirection = target.position - transform.position;
+            targetBounds.Encapsulate(target.position);
+            targetBounds.Encapsulate(target2.position);
 
-            float currentCamDist = Vector3.Distance(target.position, transform.position);
+            targetPoint = targetBounds.center;
+
+            float zoomAmount = Mathf.Max(targetBounds.size.x, targetBounds.size.y, targetBounds.size.z);
+
+            maxCameraDistance = zoomAmount + extraZoomDist;
+
+
+            transform.position = new Vector3(transform.position.x, targetPoint.y + heightAbovePlayer, transform.position.z);
+            
+            Vector3 originDirection = targetPoint - transform.position;
+
+            //targetRot = ;
+
+            float currentCamDist = Vector3.Distance(targetPoint, transform.position);
+
+
+            targetRot = Quaternion.Slerp(Quaternion.LookRotation(target.position - transform.position), Quaternion.LookRotation(originDirection), playerFocus);
+
+
+            //if (currentCamDist > maxCameraDistance)
+            //{
+            //transform.position = targetPoint + transform.forward * -maxCameraDistance;
+
+            transform.position = target.position + transform.forward * -Mathf.Lerp(20f, 30f, maxCameraDistance/40f);
+            //}
+
+            //we want it to chase the target point--this means if its too far away, we go after it!
+            //but we still want to zoom to keep an eye on characters!
+
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, .3f);
+
+
+
+
+            /*
+           
 
 
             if (!(currentCamDist < minCameraDistance))
@@ -65,7 +121,7 @@ public class CameraManager : MonoBehaviour
                 if (currentCamDist > maxCameraDistance)
                 {
 
-                    transform.position = target.position + transform.forward * -maxCameraDistance;
+                    transform.position = targetPoint + transform.forward * -maxCameraDistance;
 
                 }
             }
@@ -73,12 +129,14 @@ public class CameraManager : MonoBehaviour
             {
                 //Debug.Log("GETTIBNG CLOSE");
 
-                transform.position = target.position + Vector3.Scale(transform.forward, new Vector3(1, 1, 1)).normalized * -minCameraDistance;
+                transform.position = targetPoint + Vector3.Scale(transform.forward, new Vector3(1, 1, 1)).normalized * -minCameraDistance;
 
 
             }
 
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, .3f);
+
+            */
 
         }
 
